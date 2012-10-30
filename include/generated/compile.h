bbc608c82bb8b4b789aa3d2b870e4025471640db
GriefNorth <grief.north@gmail.com>
2012-10-30 14:49:10 +0400

    Add genlock.c genloc.h and config option's

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Oct 30 14:25:59 MSK 2012"
+#define LINUX_COMPILE_TIME "14:25:59"
+#define LINUX_COMPILE_BY "sanek"
+#define LINUX_COMPILE_HOST "desk"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
